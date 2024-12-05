54268c7760ebca0965a5ff73a7804b5ed9f9193f
yuregsf <yuregsf@protonmail.com>
2024-12-04 21:01:29 -0300

    extract funny logic to functions

--- 2024/04/index.ts
+++ 2024/04/index.ts
@@ -1,49 +1,26 @@
 import * as assert from 'assert';
 import * as fs from 'fs';
 
-function part1(input: string): number {
-  let sum = 0;
-  let lines = input.split("\n").slice(0, -1)
-  const columns = lines[0].split("").map(_ => "")
-
+function getDiagonals(lines: string[]): string[] {
   const diagonals = []
 
-  for (const line of lines) {
-    for (const j in line.split('')) {
-      columns[j] = columns[j].concat(line[j])
-    }
-  }
-
   for (const line in lines) {
     for (const col in lines[line].split('')) {
       if (+line + 3 >= lines.length || +col + 3 >= lines[line].length) break;
       diagonals.push(`${lines[line][col]}${lines[+line + 1][+col + 1]}${lines[+line + 2][+col + 2]}${lines[+line + 3][+col + 3]}`)
     }
+
   }
+  return diagonals
+}
 
-  lines = lines.reverse()
-
-  for (const line in lines) {
-    for (const col in lines[line].split('')) {
-      if (+line + 3 >= lines.length || +col + 3 >= lines[line].length) break;
-      diagonals.push(`${lines[line][col]}${lines[+line + 1][+col + 1]}${lines[+line + 2][+col + 2]}${lines[+line + 3][+col + 3]}`)
-    }
-  }
-
+function countXMAS(lines: string[]) {
+  let sum = 0;
   for (const line of lines) {
     sum += Array.from(line.matchAll(/XMAS/g)).length
     sum += Array.from(line.split('').reverse().join('').matchAll(/XMAS/g)).length
   }
 
-  for (const column of columns) {
-    sum += Array.from(column.matchAll(/XMAS/g)).length
-    sum += Array.from(column.split('').reverse().join('').matchAll(/XMAS/g)).length
-  }
-
-  for (const diagonal of diagonals) {
-    sum += Array.from(diagonal.matchAll(/XMAS/g)).length
-    sum += Array.from(diagonal.split('').reverse().join('').matchAll(/XMAS/g)).length
-  }
   return sum;
 }
 
@@ -73,6 +50,29 @@ function xmas(lines: string[]) {
   return sum;
 }
 
+function part1(input: string): number {
+  let sum = 0;
+  let lines = input.split("\n").slice(0, -1)
+  const columns = lines[0].split("").map(_ => "")
+
+  let diagonals: string[] = []
+
+  for (const line of lines) {
+    for (const j in line.split('')) {
+      columns[j] = columns[j].concat(line[j])
+    }
+  }
+
+  diagonals = diagonals.concat(getDiagonals(lines))
+  lines = lines.reverse()
+  diagonals = diagonals.concat(getDiagonals(lines))
+
+  sum += countXMAS(lines);
+  sum += countXMAS(columns);
+  sum += countXMAS(diagonals);
+
+  return sum;
+}
 
 function part2(input: string): number {
   let sum = 0;
